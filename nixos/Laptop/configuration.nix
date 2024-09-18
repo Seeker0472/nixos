c2d83ff1455b012a6705956710274bdc286aa38b
seeker <gmx472@qq.com>
2024-09-18 19:13:47 +0800

    pipewire

--- nixos/Laptop/configuration.nix
+++ nixos/Laptop/configuration.nix
@@ -40,7 +40,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
- hardware.pulseaudio.enable = true;
+ hardware.pulseaudio.enable = false;
 # sound.enable = true;
  hardware.pulseaudio.package = pkgs.pulseaudioFull;
 
@@ -50,12 +50,12 @@
 
   security.rtkit.enable = true;
  services.pipewire = {
-   enable = false;
+   enable = true;
    alsa.enable = true;
    alsa.support32Bit = true;
    pulse.enable = true;
    # If you want to use JACK applications, uncomment this
-   #jack.enable = true;
+   jack.enable = true;
 
    # use the example session manager (no others are packaged yet so this is enabled by default,
    # no need to redefine it in your config for now)
