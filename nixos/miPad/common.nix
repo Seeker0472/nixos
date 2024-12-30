43471132e0eab9f51edf0fb86efc0dc92eb11212
seeker <gmx472@qq.com>
2024-12-30 20:35:00 +0800

    add ssh for NOD

--- nixos/miPad/common.nix
+++ nixos/miPad/common.nix
@@ -4,6 +4,7 @@
     git
     curl
     unixtools.column #dep for omf
+    openssh
   ];
   # Backup etc files instead of failing to activate generation if a file already exists in /etc
   environment.etcBackupExtension = ".bak";
