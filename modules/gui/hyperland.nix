59ccf5c5de5ca55ad82405da6fdcf9624549109e
seeker <gmx472@qq.com>
2024-12-31 22:43:09 +0800

    edit hyprland keybind

--- modules/gui/hyperland.nix
+++ modules/gui/hyperland.nix
@@ -3,17 +3,18 @@ let
   copyDir = fromDir: toDir: # fromDir is a path, toDir is a string.
     lib.mapAttrs'
       (name: value: lib.nameValuePair (toDir + "/" + name)
-                                      (fromDir + "/${name}"))
+        (fromDir + "/${name}"))
       (lib.filterAttrs (name: value: value == "regular")
-                       (builtins.readDir fromDir));
+        (builtins.readDir fromDir));
   copyDirRecursively = fromDir: toDir:
     builtins.foldl'
       (a: b: a // b)
       (copyDir fromDir toDir)
-      (lib.mapAttrsToList (name: value: copyDirRecursively (fromDir + "/${name}")
-                                                           (toDir + "/" + name))
-                          (lib.filterAttrs (name: value: value == "directory")
-                                           (builtins.readDir fromDir)));
+      (lib.mapAttrsToList
+        (name: value: copyDirRecursively (fromDir + "/${name}")
+          (toDir + "/" + name))
+        (lib.filterAttrs (name: value: value == "directory")
+          (builtins.readDir fromDir)));
   mkHomeFile = fromDir: toDir:
     lib.mapAttrs'
       (name: value: lib.nameValuePair name ({ source = lib.mkDefault value; }))
@@ -21,23 +22,26 @@ let
 in
 {
   home.packages = with pkgs;[
-    hyprland-qtutils
-    hyprpaper
-    hyprlock
-    bluetuith
-    wofi
-    mako
+    hyprland-qtutils # Hyprland dependence
+    hyprpaper # wallpaper
+    hyprlock # screen-locker
+    hyprsunset # warm-color screen (TODO)
+    hyprpicker # color picker (TODO)
+    hypridle # idle management daemon (TODO screen-lock after wakeup)
+    bluetuith # bluetooth tui
+    wofi # app launcher
+    mako # notification
     pavucontrol
-    networkmanager
-    libnotify
+    networkmanager # tui network
+    libnotify # notify-send
     brightnessctl
     grim
     slurp
   ];
-  home.file = 
+  home.file =
     mkHomeFile ./hypr ".config/hypr" //
     mkHomeFile ./waybar ".config/waybar" //
     mkHomeFile ./wofi ".config/wofi" //
-    mkHomeFile ./wallpaper "Pictures/wallpaper/default" ;
+    mkHomeFile ./wallpaper "Pictures/wallpaper/default";
 
 }
