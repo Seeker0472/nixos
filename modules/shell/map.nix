fc6f098e4df5569252a3c1c3086dd23dcea85ccd
seeker <gmx472@qq.com>
2025-04-05 23:44:30 +0800

    add scripts mapping

--- modules/shell/map.nix
+++ modules/shell/map.nix
@@ -2,7 +2,11 @@
 let
   nvimconfig_path =
     "${config.home.homeDirectory}/nixos-config/modules/shell/neovim";
+  scripts_path =
+    "${config.home.homeDirectory}/nixos-config/modules/shell/scripts";
 in {
   home.file.".config/nvim".source =
     config.lib.file.mkOutOfStoreSymlink nvimconfig_path;
+  home.file."scripts".source =
+    config.lib.file.mkOutOfStoreSymlink scripts_path;
 }
