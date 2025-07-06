c670721dbb4cab4e4cf1dd22304663923c9612ef
seeker <gmx472@qq.com>
2025-07-07 01:06:01 +0800

    update fish shell prompt,support python vnev,nix,git

--- modules/shell/fish.nix
+++ modules/shell/fish.nix
@@ -7,6 +7,7 @@
       set theme_date_format +"%b/%e|%a %H:%M.%S"
       function fish_greeting; end
       set -g fish_key_bindings fish_vi_key_bindings
+      set -g VIRTUAL_ENV_DISABLE_PROMPT 1
 
       # --- for yazi ---
       function y
@@ -71,23 +72,56 @@
       '';
 
       fish_prompt = ''
-        set -l last_status $status
 
-        # 定义颜色
-        set -l color_mode_insert_bg magenta
-        set -l color_mode_normal_bg blue
-        set -l color_mode_visual_bg blue
+        # --- 定义颜色 ---
+        set -l color_mode_insert_bg blue
+        set -l color_mode_normal_bg magenta
+        set -l color_mode_visual_bg green
         set -l color_mode_replace_bg blue
         set -l color_mode_default_bg white
 
         # set -l color_success_bg green
         set -l color_error_bg red
         set -l color_path_bg cyan
-        set -l color_git_bg yellow
+        set -l color_git_bg yellow # maybe more color!
+        set -l color_nix_bg green
+        set -l color_venv_bg red
+        
+        # --- 计算各段颜色 ---
+        set -l last_status $status
 
         if test $last_status -eq 0
+          set _color_path_bg $color_path_bg
         else
-          set color_path_bg $color_error_bg
+          set _color_path_bg $color_error_bg
+        end
+
+        # Git exists
+        if git rev-parse --is-inside-work-tree >/dev/null 2>&1
+          set _color_git_bg $color_git_bg
+        else
+          set _color_git_bg normal
+        end
+        
+        # Nix exists(direnv or nix-shell)
+        if test -n "$IN_NIX_SHELL"; or test -n "$DIRENV_DIR"
+          set _color_nix_bg $color_nix_bg
+          set _nix_prompt "󱄅"
+          if test -n "$IN_NIX_SHELL" 
+            set _nix_prompt "$_nix_prompt "
+          end
+          if test -n "$DIRENV_DIR"
+            set _nix_prompt "$_nix_prompt "
+          end
+        else
+          set _color_nix_bg $_color_git_bg
+        end
+
+        # Venv exists
+        if test -n "$VIRTUAL_ENV"
+          set _color_venv_bg $color_venv_bg
+        else
+          set _color_venv_bg $_color_nix_bg
         end
 
         # --- 绘制PROMPT ---
@@ -116,7 +150,7 @@
                 set mode_indicator $fish_bind_mode
         end
 
-        _prompt_segment $mode_bg $color_path_bg "$mode_indicator"
+        _prompt_segment $mode_bg $_color_path_bg "$mode_indicator"
         
         # 现在感觉 [ 状态 ]段没有用,把上一条指令的结果显示在路径的背景颜色更高效x
         # 如果模式段的下一个是状态段,就启用这些
@@ -124,19 +158,29 @@
         # _prompt_segment $mode_bg $next_bg_for_mode "$mode_indicator"
         # # 2. [ 状态 ] 段
         # if test $last_status -eq 0
-        #     _prompt_segment $color_success_bg $color_path_bg "✔"
+        #     _prompt_segment $color_success_bg $_color_path_bg "✔"
         # else
-        #     _prompt_segment $color_error_bg $color_path_bg "✘"
+        #     _prompt_segment $color_error_bg $_color_path_bg "✘"
         # end
 
         # 3. [ 路径 ] 段
-        set -l next_bg_for_path (if git rev-parse --is-inside-work-tree >/dev/null 2>&1; echo $color_git_bg; else; echo normal; end)
-        _prompt_segment $color_path_bg $next_bg_for_path (prompt_pwd)
+        _prompt_segment $_color_path_bg $_color_venv_bg (prompt_pwd)
 
-        # 4. [ Git ] 段 
+        # 4. [ Python venv ]
+        if test -n "$VIRTUAL_ENV"
+          set -l venv_basename (basename "$VIRTUAL_ENV")
+          _prompt_segment $color_venv_bg $_color_nix_bg " $venv_basename"
+        end
+        
+        # 5. [ Nix-venv or Nix-shell ]
+        if test -n "$IN_NIX_SHELL"; or test -n "$DIRENV_DIR"
+          _prompt_segment $color_nix_bg $_color_git_bg $_nix_prompt
+        end
+
+        # 6. [ Git ]
         _print_git_segment $color_git_bg
 
-        # 5. Prompt 结尾
+        # 7. Prompt 结尾
         set_color normal
         echo -n " "
       '';
